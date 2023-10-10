38c644aa137452a57a91cb4de6b0e3cc1a3697cc
Mauricio Miranda <mauriciotresa@gmail.com>
2023-10-10 01:47:42 -0300

    creo un mapeo del ruter, creo las carpetas controller model y view con los archivos correspondientes y la carpeta template con un footer y un header tentativo, subo la db

---
+++ router.php
@@ -0,0 +1,66 @@
+<?php
+
+include_once '/controller/game.controller.php';
+
+// defino la base url para la construccion de links con urls semánticas
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+
+
+if(!empty($_GET['action'])){    
+    $action = $_GET['action'];
+} else {
+    $action = 'inicio';
+}
+
+$params = explode('/', $action);
+
+switch($params[0]){
+    case 'inicio':
+        //muestra home
+            
+        break;
+    case 'quienes somos':
+        //descripcion de la pagina  + desarrolladores (Rod - juli - Mauricio)
+              
+        break;
+    case 'busquedas':
+        // diferentes tipos de busqueda (los que pide el enunciado)
+            
+        break;
+    case 'agregarJuego':
+        // Agrega un juego a la db - solo administrador logeado
+        
+        break;
+    case 'borrarJuego':
+        // Borra un juego de la db - solo administrador logeado
+        break;
+        
+    case 'modificaJuego':
+        // Modifica un juego en la db - solo administrador logeado
+        break;
+
+    case 'agregadesarrolladora':
+        // Agrega una desarrolladora a la db - solo administrador logeado
+        break;
+
+    case 'borradesarrolladora':
+        // Borra una desarrolladora de la db - solo administrador logeado
+        break;
+
+    case 'modificadesarrolladora':
+        // Modifica una desarrolladora de la db - solo administrador logeado
+        break;
+
+    case 'login':
+        // acceso de administrador
+        break;
+
+    case 'logout':
+        // deslogueo administrador
+        break;
+           
+    default:
+        echo " ERROR 404 - Pagina no encontrada"; // este error tiene que salir de la vista
+        break;
+}
